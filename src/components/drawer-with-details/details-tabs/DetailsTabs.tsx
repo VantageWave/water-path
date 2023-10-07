fbbb988e25003b66fedc71deccc30dd5ebbec8de
Kamil Leśniak <115118024+lesnik2u@users.noreply.github.com>
2023-10-07 22:29:49 +0200

    Language picker (#39)
    * changed build method
    
    * added message definitions
    
    * removed babelrc
    
    * added missing import
    
    * fixed import issue
    
    * translations
    
    * translation and css fixes
    
    ---------
    
    Co-authored-by: jczuchra <36820932+jczuchra@users.noreply.github.com>

--- src/components/drawer-with-details/details-tabs/DetailsTabs.tsx
+++ src/components/drawer-with-details/details-tabs/DetailsTabs.tsx
@@ -3,8 +3,10 @@ import InformationTab from './information-tab/InformationTab';
 import NewsTab from './news-tab/NewsTab';
 import type { DetailsTabsProps } from './DetailsTabs.types';
 import DataTab from './data-tab/DataTab';
+import { defineMessages, useIntl } from 'react-intl';
 
 const DetailsTabs = ({ level, className }: DetailsTabsProps) => {
+  const { formatMessage } = useIntl();
   return (
     <Tabs
       color="primary"
@@ -13,17 +15,40 @@ const DetailsTabs = ({ level, className }: DetailsTabsProps) => {
       className={className}
       fullWidth
     >
-      <Tab key="info" title="Information" className="h-[100%]">
+      <Tab
+        key="info"
+        title={formatMessage(messages.information)}
+        className="h-[100%]"
+      >
         <InformationTab level={level} />
       </Tab>
-      <Tab key="news" title="News" className="h-[100%]">
+      <Tab key="news" title={formatMessage(messages.news)} className="h-[100%]">
         <NewsTab level={level} />
       </Tab>
-      <Tab key="source" title="Data" className="h-[100%]">
+      <Tab
+        key="source"
+        title={formatMessage(messages.source)}
+        className="h-[100%]"
+      >
         <DataTab level={level} />
       </Tab>
     </Tabs>
   );
 };
+
+const messages = defineMessages({
+  information: {
+    id: 'src.components.drawer-with-details.information',
+    defaultMessage: 'Information',
+  },
+  news: {
+    id: 'src.components.drawer-with-details.news',
+    defaultMessage: 'News',
+  },
+  source: {
+    id: 'src.components.drawer-with-details.source',
+    defaultMessage: 'Source ',
+  },
+});
 
 export default DetailsTabs;
