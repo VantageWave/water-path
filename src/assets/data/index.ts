2c78215216d7b7b29e0bd6f26845927703a519f3
jczuchra <36820932+jczuchra@users.noreply.github.com>
2023-10-07 15:31:40 +0200

    feat: update data (#19)
    * feat: update-data
    
    * fix: remove package

---
+++ src/assets/data/index.ts
@@ -0,0 +1,5 @@
+import wilczynskieLake from "./wilczynskieLake.json";
+
+export default {
+    1: wilczynskieLake,
+}
--- src/components/ImageComparator/ImageComparator.tsx
+++ src/components/ImageComparator/ImageComparator.tsx
@@ -1,24 +1,29 @@
 import { useContext } from "react";
-import ReactCompareImage from 'react-compare-image';
-import Cat1 from "./images/cat1.jpg";
-import Cat2 from "./images/cat2.jpg";
-import monkey1 from "./images/monkey1.jpg";
-import monkey2 from "./images/monkey2.jpg";
-import landscape1 from "./images/landscape1.jpg";
-import landscape2 from "./images/landscape2.jpg";
 import { DataContext } from '../../context';
+import { ReactCompareImage } from "../CompareImage"
+import waterData from "../../assets/data";
 
 const data = {
-  1: [Cat1, Cat2],
-  2: [monkey1, monkey2],
-  3: [landscape1, landscape2],
+  1: waterData[1],
+  // 2: [monkey1, monkey2],
+  // 3: [landscape1, landscape2],
 }
-
 const ImageComparator = () => {
   const { state } = useContext(DataContext);
   const currentCase = state.case as keyof typeof data;
   const currentData = data[currentCase] || data[1];
-  return <ReactCompareImage leftImage={currentData[0]} rightImage={currentData[1]} />;;
+
+  return (
+    <div className="relative border-r">
+      <ReactCompareImage
+        leftImage={currentData[0].filename}
+        rightImage={currentData[1].filename}
+        leftImageLabel={`${currentData[0].metadata.area} ${currentData[0].metadata.unit}`}
+        rightImageLabel={`${currentData[1].metadata.area} ${currentData[1].metadata.unit}`}
+        leftLabelClass="rounded-md"
+        rightLabelClass="rounded-md"
+      />
+    </div>)
 };
 
 export default ImageComparator;
