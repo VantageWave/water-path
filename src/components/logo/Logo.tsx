cdbe685dc7051798494b04feecf65884531ce1dc
Rafal Grabianski <rafal.grabianski@cdex.cloud>
2023-10-07 13:48:38 +0200

    Add better logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,6 +2,18 @@ import { Tooltip } from '@nextui-org/react';
 import { LogoProps } from './Logo.types';
 import projectLogo from '../../assets/logo.png';
 
+// LogoContainer that has black background and rounded edges
+const LogoContainer = ({ className }: any) => {
+  return (
+    <div
+      className={`flex flex-row items-center justify-center bg-black rounded-md opacity-40 ${className}`}
+      style={{ padding: '4px 8px' }}
+    >
+      <strong>Water Path&nbsp;</strong>{"@ 2023"}
+    </div>
+  );
+}
+
 const Logo = ({ className }: LogoProps) => {
   return (
     <footer className={`flex flex-col items-center gap-[6px] ${className}`}>
@@ -9,9 +21,7 @@ const Logo = ({ className }: LogoProps) => {
         <img src={projectLogo} alt="SmartPath" className="w-[60px] h-[60px]" />
       </Tooltip>
 
-      <span className="text-[10px]">
-        <strong>Water Path</strong> @ 2023
-      </span>
+      <LogoContainer className="text-[12px]" />        
     </footer>
   );
 };
