b049b17947bd834e366add00dc56e160c29a84d6
Kamil Leśniak <115118024+lesnik2u@users.noreply.github.com>
2023-10-07 15:54:31 +0200

    Date picker (#20)
    * date selector
    
    * oswiadczenie
    
    ---------
    
    Co-authored-by: jczuchra <czuchra.jakub@gmail.com>

--- src/components/date-selector/DateSelector.tsx
+++ src/components/date-selector/DateSelector.tsx
@@ -1,10 +1,93 @@
-import { Card } from '@nextui-org/react';
-import { DateSelectorProps } from './DateSelector.types';
+import {
+  Card,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  CardBody,
+} from '@nextui-org/react';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+
+import { DateSelectorProps, dateSelector } from './DateSelector.types';
+import { useMemo, useState } from 'react';
+
+import data from '../../assets/data/data.json';
+
+const DropdownSelector = ({
+  text,
+  selectedStartDate,
+  onStartDateChange,
+}: dateSelector) => {
+  const [selectedKey, setselectedKey] = useState<Set<string>>(new Set([text]));
+
+  const selectedValue = useMemo(() => Array.from(selectedKey), [selectedKey]);
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        {selectedStartDate === '' ? (
+          <Button variant="bordered" className="capitalize" isDisabled>
+            {selectedValue}
+            <CalendarTodayIcon fontSize="small" />
+          </Button>
+        ) : (
+          <Button variant="bordered" className="capitalize px-5">
+            {selectedValue}
+            <CalendarTodayIcon fontSize="small" />
+          </Button>
+        )}
+      </DropdownTrigger>
+      <DropdownMenu
+        variant="flat"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKey={selectedKey}
+        onSelectionChange={(newselectedKey: Set<string>) => {
+          setselectedKey(newselectedKey);
+          if (onStartDateChange && newselectedKey.size === 1) {
+            const startDate = Array.from(newselectedKey)[0];
+            onStartDateChange(startDate);
+          }
+        }}
+        className="text-center"
+      >
+        {selectedStartDate === 'any'
+          ? data
+              .slice(0, -1)
+              .map((ob) => <DropdownItem key={ob.date}>{ob.date}</DropdownItem>)
+          : data
+              .filter((item) => item.date > selectedStartDate)
+              .map((ob) => (
+                <DropdownItem key={ob.date}>{ob.date}</DropdownItem>
+              ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 
 const DateSelector = ({ className }: DateSelectorProps) => {
+  const [selectedStartDate, setSelectedStartDate] = useState<string>('');
   return (
-    <Card className={`${className} w-[600px] h-[150px] dark:bg-foreground-100`}>
-      Date picker here
+    <Card className={`${className} dark:bg-foreground-100/80`}>
+      <CardBody className="p-1">
+        <div className="flex items-center justify-center gap-2">
+          <DropdownSelector
+            text="Start date"
+            selectedStartDate={'any'}
+            onStartDateChange={(startDate) => setSelectedStartDate(startDate)}
+          />
+          <KeyboardDoubleArrowRightIcon
+            fontSize="large"
+            className="basis-1/3"
+          />
+          <DropdownSelector
+            text="End date"
+            selectedStartDate={selectedStartDate}
+            onStartDateChange={() => null}
+          />
+        </div>
+      </CardBody>
     </Card>
   );
 };
--- src/components/date-selector/DateSelector.types.ts
+++ src/components/date-selector/DateSelector.types.ts
@@ -1,3 +1,9 @@
 export interface DateSelectorProps {
   className: string;
 }
+
+export interface dateSelector {
+  text: string;
+  selectedStartDate: string;
+  onStartDateChange: (startDate: string) => void;
+}
