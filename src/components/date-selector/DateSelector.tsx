121b5575a1d044edfcaf637866a46ec789276be0
Rafal Grabianski <rafal.grabianski@cdex.cloud>
2023-10-07 18:43:45 +0200

    Fix paddings

--- src/components/date-selector/DateSelector.tsx
+++ src/components/date-selector/DateSelector.tsx
@@ -30,7 +30,7 @@ const DropdownSelector = ({
   const selectedValue = useMemo(() => Array.from(selectedKey), [selectedKey]);
   return (
     <Dropdown>
-      <DropdownTrigger>
+      <DropdownTrigger className="px-2">
         {selectedStartDate === '' ? (
           <Button variant="bordered" className="capitalize" isDisabled>
             {selectedValue}
@@ -87,9 +87,9 @@ const DropdownSelector = ({
 const DateSelector = ({ className }: DateSelectorProps) => {
   const [selectedStartDate, setSelectedStartDate] = useState<string>('');
   return (
-    <Card className={`${className} dark:bg-foreground-100/80 px-3`}>
-      <CardBody className="p-1">
-        <div className="flex items-center justify-center gap-2">
+    <Card className={`${className} dark:bg-foreground-100/80 px-0 py-1`}>
+      <CardBody className="p-0">
+        <div className="items-center justify-center gap-2">
           <DropdownSelector
             text="Start date"
             selectedStartDate={'any'}
