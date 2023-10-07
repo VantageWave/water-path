51dee78e049f3343e38541604ddf484dbba7d3ad
jczuchra <36820932+jczuchra@users.noreply.github.com>
2023-10-07 22:34:24 +0200

    feat: visual adjustments (#40)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { defineMessages, useIntl } from 'react-intl';
 import "./homePages.css";
+import { LanguagePicker } from '../components/LanguagePicker/languagePicker';
 
 export const HomePage = () => {
+    const { formatMessage } = useIntl();
     const waves = [];
 
     for (let i = 0; i < 480; i++) {
-        waves.push(<div className="line" />)
+        waves.push(<div className="line" key={`wave-${i}`} />)
     }
     return (
-        <>
+        <motion.main
+            className="main__container"
+            initial={{ width: 0 }}
+            animate={{ width: "100%" }}
+            exit={{ x: "100%", opacity: 0 }}
+            transition={{ duration: 0.6 }}
+        >
+            <LanguagePicker isTop />
             <div id="container">
-                <button className="learn-more">
-                    <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                    </span>
-                    <span className="button-text">Learn More</span>
-                </button>
+                <Link to="/waterPath" className="learn-more">
+                    <button className="learn-more">
+                        <span className="circle" aria-hidden="true">
+                            <span className="icon arrow"></span>
+                        </span>
+                        <span className="button-text">{formatMessage(messages.explore)}</span>
+                    </button>
+                </Link>
             </div>
             <h1 className="title">
                 Water Path
@@ -23,6 +37,13 @@ export const HomePage = () => {
             <div className="container">
                 {waves}
             </div>
-        </>
+        </motion.main>
     )
 }
+
+const messages = defineMessages({
+    explore: {
+      id: 'src.pages.HomePage.explore',
+      defaultMessage: 'Explore',
+    },
+  });
--- src/pages/WaterPage.tsx
+++ src/pages/WaterPage.tsx
@@ -1,14 +1,23 @@
+import { motion } from "framer-motion";
 import ImageComparator from '../components/ImageComparator/ImageComparator';
 import CaseSelector from '../components/CaseSelector/CaseSelector';
 import DrawerWithDetails from '../components/drawer-with-details/DrawerWithDetails';
 import DateSelector from '../components/date-selector/DateSelector';
 import Logo from '../components/logo/Logo';
+import { LanguagePicker } from '../components/LanguagePicker/languagePicker';
 
 export const WaterPage = () => (
-    <>
+    <motion.main
+        className="main__container"
+        initial={{ width: 0 }}
+        animate={{ width: "100%" }}
+        exit={{ x: "100%", opacity: 0 }}
+        transition={{ duration: 0.6 }}
+    >
         <CaseSelector className="max-w-xs absolute left-3 top-3 z-10" />
         <ImageComparator />
         <Logo className="absolute left-3 bottom-3" />
         <DateSelector className="absolute bottom-3 left-1/2 transform -translate-x-1/2" />
         <DrawerWithDetails />
-    </>)
+        <LanguagePicker />
+    </motion.main>)
