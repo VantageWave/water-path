f95d6b2e19f7c9eafe49ee07568ff2ac8632815b
Szymon Rafałowski <53696262+szyraf@users.noreply.github.com>
2023-10-08 10:10:45 +0200

    added miles for americans (#46)
    * added miles for americans
    
    * fix

--- src/components/ImageComparator/ImageComparator.tsx
+++ src/components/ImageComparator/ImageComparator.tsx
@@ -21,12 +21,10 @@ const ImageComparator = () => {
       <ReactCompareImage
         leftImage={startData?.filename || ''}
         rightImage={endData?.filename || ''}
-        leftImageLabel={`${startData?.metadata.area || '0'} ${
-          startData?.metadata.unit || 'km'
-        }`}
-        rightImageLabel={`${endData?.metadata.area || '0'} ${
-          endData?.metadata.unit || 'km'
-        }`}
+        leftImageLabel={`${state.lang === 'en' ? ((startData?.metadata.area || 0) / (1.60934 * 1.60934)).toFixed(4) : (startData?.metadata.area || '0')} ${state.lang === 'en' ? 'mi' : (endData?.metadata.unit || 'km')
+          }`}
+        rightImageLabel={`${state.lang === 'en' ? ((endData?.metadata.area || 0) / (1.60934 * 1.60934)).toFixed(4) : (endData?.metadata.area || '0')} ${state.lang === 'en' ? 'mi' : (endData?.metadata.unit || 'km')
+          }`}
         leftLabelClass="rounded-md"
         rightLabelClass="rounded-md"
       />
