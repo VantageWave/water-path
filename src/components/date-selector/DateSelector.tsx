1e5733774230310994b842c7b3605e15db37bfad
jczuchra <czuchra.jakub@gmail.com>
2023-10-08 12:43:12 +0200

    fix: selector bug

--- src/components/date-selector/DateSelector.tsx
+++ src/components/date-selector/DateSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   Card,
   Dropdown,
@@ -26,6 +26,10 @@ const DropdownSelector = ({
   const data = allData[state.case];
 
   const { dispatch } = useContext(DataContext);
+
+  useEffect(() => {
+    setSelectedKey(null);
+  }, [state.case]);
 
   return (
     <Dropdown>
